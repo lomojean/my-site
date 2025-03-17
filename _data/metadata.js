f80cfda2db69fb259d81bd32d586f0fa6009b0cf
Lomo Jean <jfleming@gmail.com>
2025-03-17 13:28:36 -0700

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Jennifer's Eleventy Base Blog 🤝 Fastly Compute",
+	title: "Eleventy Base Blog 🤝 Fastly Compute",
 	url: "https://glitchdotcom.github.io/my-site/",
 	language: "en",
 	description: "An example 11ty blog website for experimenting with Fastly Compute.",
 	author: {
-e		name: "Jennifer Fleming",
-		email: "jfleming@fastly.com",
+		name: "Your Name Here",
+		email: "youremailaddress@example.com",
 		url: "https://example.com/about-me/"
 	}
 }
